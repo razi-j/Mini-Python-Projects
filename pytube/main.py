e5bc5ca2f0c452b8dba8a3079d53f50596972960
Jaja <83009990+razi-j@users.noreply.github.com>
2022-08-07 23:09:20 +0800

    Added filtered stream functionality

--- pytube/main.py
+++ pytube/main.py
@@ -1,18 +1,21 @@
 from pytube import YouTube
 
-rs = ["a", "v"]
-
 def download(url):
     yt = YouTube(url)
+    print("{}".format(yt.title))
     ch = str(input("Download as Mp4 (v) or as Audio (a)?: "))
     
-    if ch.lower not in rs:
-        print("Invalid Input")
-    elif ch.lower() == "a":
+    if ch.lower() == "a":
         filtered = yt.streams.filter(only_audio=True)
         streams = list(enumerate(filtered))
         for i in streams:
             print(i)
+    elif ch.lower() == "v":
+        filtered = yt.streams.filter(file_extension="mp4", only_video=True)
+        streams = list(enumerate(filtered))
+        for i in streams:
+            print(i)
+    else: print("Invalid Input")
 
 if __name__ == "__main__":
     link = str(input("YT Video Link: "))
